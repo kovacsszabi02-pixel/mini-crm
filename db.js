9aef96e96a29670ffd92feaf79413bef044a3115
Kovács Szabolcs <kovacsszabolcs@Kovacs-MacBook-Air-2.local>
2026-09-16 21:49:38 +0200

    Fix database connection string config

--- db.js
+++ db.js
@@ -1,16 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres.pcmhyudzgxxhxkytelha:qInwiw-3byzgi-pansuw@aws-0-eu-central-1.pooler.supabase.com:5432/postgres',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
-
-pool.on('error', (err) => {
-  console.error('Váratlan hiba az adatbázis-kapcsolatban:', err);
-});
-
-console.log('Sikeresen csatlakozva a Supabase felhő adatbázishoz!');
 
 module.exports = pool;
